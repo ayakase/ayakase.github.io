2b4973db85c16ecf580367eff918e09e70eec179
ayakase <wibu1892001@gmail.com>
2023-01-08 15:31:57 +0700

    99% done

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-/* setInterval(function () {
+/* TO AUTOMATICALLY SCROLL TO BOTTOM WHILE RELOADING  LIVE SERVERS, ONNLY USED WHEN DEVELOPING
+ setInterval(function () {
     window.scrollTo(0, document.body.scrollHeight)
 }, 500)
 */
+
+/* NOTE: gotta use resize observer if needed for responsive*/
