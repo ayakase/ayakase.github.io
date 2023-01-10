0b4c37b36f973040e2dbdadba5385ff07c0272aa
tokisaki189 <andt@runsystem.net>
2023-01-10 09:47:49 +0700

    test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* TO AUTOMATICALLY SCROLL TO BOTTOM WHILE RELOADING  LIVE SERVERS, ONNLY USED WHEN DEVELOPING
+/* TO AUTOMATICALLY SCROLL TO BOTTOM WHILE RELOADING  LIVE SERVERS, ONLY USED WHEN DEVELOPING
  setInterval(function () {
     window.scrollTo(0, document.body.scrollHeight)
 }, 500)
@@ -7,9 +7,9 @@
 /* NOTE: gotta use resize observer if needed for responsive*/
 
 window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementsByClassName("header-main")[0].style.fontSize = "20px";
-    }
-}
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".header-main");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
